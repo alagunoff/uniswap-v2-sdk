2c92bbc0f7c44a6e8a03e365c22310ce8169e190
alagunoff <lagunov.artamon@gmail.com>
2024-12-28 13:11:28 +0300

    feat: remove tests; add prettier

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-export { FACTORY_ADDRESS, INIT_CODE_HASH, MINIMUM_LIQUIDITY } from './constants'
+export {
+  FACTORY_ADDRESS,
+  INIT_CODE_HASH,
+  MINIMUM_LIQUIDITY,
+} from './constants';
 
-export * from './errors'
-export * from './entities'
-export * from './router'
+export * from './errors';
+export * from './entities';
+export * from './router';
